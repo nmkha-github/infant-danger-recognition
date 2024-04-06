fix relu gradient

components/ResnetGCN/ResnetGCN.py
@@ -23,9 +23,9 @@ class ResidualBlock(nn.Module):
     def forward(self, x, edge_index):
         identity = x
         x = self.bn1(self.conv1(x, edge_index))
-        x = F.relu(x)
+        x = torch.relu(x.clone())
         x = self.bn2(self.conv2(x, edge_index))
-        x = F.relu(x)
+        x = torch.relu(x.clone())
 
         if self.downsample_conv is not None:
             identity = self.downsample_bn(
@@ -64,7 +64,7 @@ class ResnetGCN(nn.Module):
         data.x = F.normalize(data.x, p=2, dim=0)
         data = data.to(self.device)
 
-        out = F.relu(self.conv1(data.x, data.edge_index))
+        out = torch.relu(self.conv1(data.x, data.edge_index).clone())
         for block in self.blocks:
             out = block(out, data.edge_index)
         out = self.conv2(out, data.edge_index)

scripts/GCNModel/train_gcn_model.py
@@ -19,7 +19,6 @@ import warnings
 
 warnings.filterwarnings("ignore")
 
-
 # Prepare data
 video_folder_path = os.path.join(project_path, "data/Short_Videos")
 train_excel_path = os.path.join(project_path, "data/Short_Videos/annotation/train.xlsx")
@@ -114,7 +113,7 @@ while True:
 
             outputs_action = model(
                 graph=graph
-            )  # example output: tensor([0.1, 0.2, 0.3, 0.2, 0.2]) and tensor([0.443])
+            )  # example output: tensor([0.1, 0.2, 0.3, 0.2, 0.2])
             outputs_action = outputs_action.unsqueeze(0)
             batch_outputs_action = torch.cat((batch_outputs_action, outputs_action), 0)
 